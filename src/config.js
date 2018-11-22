4694d4cfcdcec234d204e7e720ab1530732c0fcb
Mariano Rivas <mrivas@Colormono.local>
2018-11-22 15:03:16 -0300

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.WEBGL,
+  parent: 'content',
+  width: 800,
+  height: 600,
+  localStorageName: 'phaseres6webpack'
+};
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [path.resolve(__dirname, 'src/index.js')]
+    //vendor: ['phaser']
+  },
+  mode: 'development',
+  output: {
+    pathinfo: true,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './dist/',
+    filename: 'bundle.js'
+  },
+  watch: true,
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    }),
+    //new CopyWebpackPlugin([{ from: './src/assets', to: './assets' }]),
+    new BrowserSyncPlugin({
+      host: process.env.IP || 'localhost',
+      port: process.env.PORT || 3000,
+      server: {
+        baseDir: ['./', './build']
+      }
+    })
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
